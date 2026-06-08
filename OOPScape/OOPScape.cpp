dbca83e1283691bc201baaa7b7a3b02c79a561d4
ikoleva <iii_koleva@abv.bg>
2026-06-08 17:36:20 +0300

    Add 10 levels and implement level selection

--- OOPScape/OOPScape.cpp
+++ OOPScape/OOPScape.cpp
@@ -7,9 +7,34 @@
 
 int main()
 {
+    int levelChoice;
+
+    std::cout << "Choose level:" << std::endl;
+    std::cout << "1. Level 1" << std::endl;
+    std::cout << "2. Level 2" << std::endl;
+    std::cout << "3. Level 3" << std::endl;
+    std::cout << "4. Level 4" << std::endl;
+    std::cout << "5. Level 5" << std::endl;
+    std::cout << "6. Level 6" << std::endl;
+    std::cout << "7. Level 7" << std::endl;
+    std::cout << "8. Level 8" << std::endl;
+    std::cout << "9. Level 9" << std::endl;
+    std::cout << "10. Level 10" << std::endl;
+    std::cout << "Choice: ";
+
+    std::cin >> levelChoice;
+
+    if (levelChoice < 1 || levelChoice > 10)
+    {
+        std::cout << "Invalid level choice!" << std::endl;
+        return 1;
+    }
+
+    std::string levelFileName = "levels/level" + std::to_string(levelChoice) + ".txt";
+
     Game game;
 
-    if (!game.loadLevel("levels/level1.txt"))
+    if (!game.loadLevel(levelFileName))
     {
         return 1;
     }
